04d3a83b7e657a7e7a30dd353dc843eff7478083
Angel PCGM <angel.gomez.t89@gmail.com>
2021-12-11 10:27:18 -0600

    Comentarios de realizacion

--- nodejs/app.js
+++ nodejs/app.js
@@ -22,3 +22,9 @@ app.get('/municipios', (req, res) => {
 app.listen(3000, () => {
     console.log('Servidor listo .....');
 });
+//1.- Aceder a la informacion proporcionada por la INEGI (error)
+//2.- Buscar primero el estado "https://gaia.inegi.org.mx/wscatgeo/geo/mgee/buscar/aguas"
+//3.- Si no lo encuentra buscar la Localidad "https://gaia.inegi.org.mx/wscatgeo/geo/mgem/buscar/aguas"
+//*Cualquiera de estos me genera o me trae la cordenadas de busqueda.(Si es que no hago la conexion, no tengo los elementos de tenerlo ya puedo manipularlos)
+//4.- Fucionarlo con el negocio y hacer la busqueda "https://www.inegi.org.mx/app/api/denue/v1/consulta/Buscar/camiones/21.85717833,-102.28487238/250/[aquí va tu Token]"
+//*Si se se cuenta con varias cordenadas meterlo en un ForEach y de esta manera mostrarlo en la paguina
